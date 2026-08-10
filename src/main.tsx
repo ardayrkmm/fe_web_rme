85662175425ebbae5c44ddc46d4c1b0d694e8d53
ardayrkmm <ardyrkm23@gmail.com>
2026-08-10 11:22:37 +0700

    Fix error removeChild (Google Translate fix)k

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// --- Patch untuk mencegah error dari Google Translate / Ekstensi Pihak Ketiga ---
+if (typeof Node === 'function' && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function (child) {
+    if (child.parentNode !== this) {
+      console.warn('Dicegah crash oleh ekstensi pihak ketiga (removeChild):', child);
+      return child;
+    }
+    return originalRemoveChild.apply(this, arguments as any);
+  };
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function (newNode, referenceNode) {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      console.warn('Dicegah crash oleh ekstensi pihak ketiga (insertBefore):', referenceNode);
+      return newNode;
+    }
+    return originalInsertBefore.apply(this, arguments as any);
+  };
+}
+// -----------------------------------------------------------------------------
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
